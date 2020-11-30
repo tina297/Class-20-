2f68b62daf142bca76cf6ccd741e48981af19990
tina297 <74814611+tina297@users.noreply.github.com>
2020-11-30 18:05:08 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() { createCanvas(1200,800);
+     fixedRect= createSprite(200, 200, 50, 80);
+     fixedRect.shapeColor="green"; 
+     //fixedRect.debug=true 
+     movingRect=createSprite(400,200,80,30); 
+     movingRect.shapeColor="green"; 
+     //movingRect.debug=true; }
+}
+
+
+
+
+function draw() {
+  background(255,255,255);  
+movingRect.x = mouseX;
+movingRect.y = mouseY;
+
+if(movingRect.x - fixedRect.x<fixedRect.width/2 + movingRect.width/2
+  &&fixedRect.x - movingRect.x<fixedRect.width/2 + movingRect.width/2
+  &&movingRect.y - fixedRect.y<fixedRect.height/2 + movingRect.height/2
+  &&fixedRect.y - movingRect.y<fixedRect.height/2 + movingRect.height/2) {
+  movingRect.shapeColor = "red";
+  fixedRect.shapeColor = "red";
+}
+else {
+  movingRect.shapeColor = "green";
+  fixedRect.shapeColor = "green";
+}
+
+
+
+  drawSprites();
+}
